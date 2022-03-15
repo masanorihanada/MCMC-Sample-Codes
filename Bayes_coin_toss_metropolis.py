3073be550387ad7dd42666604ea0ecb7660d75e0
Masanori Hanada <37608759+masanorihanada@users.noreply.github.com>
2022-03-15 23:32:31 +0000

    Update Bayes_coin_toss_metropolis.py

--- Bayes_coin_toss_metropolis.py
+++ Bayes_coin_toss_metropolis.py
@@ -19,15 +19,18 @@ for ite in range(1,NITER):
   dp = (dp-0.5)*step_size*2.0
   p = p + dp
 
-  action_fin = -515.0*math.log(p)-485.0*math.log(1-p)+100.0*(p-0.9)**2
-
-  # metropolis test 
-  metropolis = np.random.rand()
-  if(np.exp(action_init - action_fin) > metropolis):
-    #accept
-    naccept=naccept+1
-  else:
+  if( p<0 or p>1 ): # automatically reject
     p=backup_p
+  else:
+    action_fin = -515.0*math.log(p)-485.0*math.log(1-p)+100.0*(p-0.9)**2
+  
+    # metropolis test 
+    metropolis = np.random.rand()
+    if(np.exp(action_init - action_fin) > metropolis):
+      #accept
+      naccept=naccept+1
+    else:
+      p=backup_p
 
   if( ite >= 100): # discard first 100 samples for thermalization
     p_hist.append(p)
